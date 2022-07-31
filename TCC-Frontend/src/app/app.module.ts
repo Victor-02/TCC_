d1d6953190d0c875e6eba18e8aa4d294ce8c0cdb
Victor Casemiro <victorcasemiro2002@hotmail.com>
2022-07-31 14:37:35 -0300

    criando a view importação

--- TCC-Frontend/src/app/app.module.ts
+++ TCC-Frontend/src/app/app.module.ts
@@ -9,9 +9,10 @@ import { HomeComponent } from './view/home/home.component';
 import { MatImportsModule } from './shared/mat-imports/mat-imports.module';
 import { PaginatorComponent } from './shared/paginator/paginator.component';
 import { AgendamentoComponent } from './view/agendamento/agendamento.component';
+import { ImportacaoComponent } from './view/importacao/importacao.component';
 
 @NgModule({
-    declarations: [AppComponent, HomeComponent, AgendamentoComponent],
+    declarations: [AppComponent, HomeComponent, AgendamentoComponent, ImportacaoComponent],
     imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatImportsModule, HttpClientModule],
     providers: [],
     bootstrap: [AppComponent],
