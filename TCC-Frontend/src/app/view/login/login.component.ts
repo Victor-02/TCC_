09ebff6067c8311fe5becaeae349c62e74ad2311
Victor Casemiro <victorcasemiro2002@hotmail.com>
2022-08-03 23:07:16 -0300

    fazendo o ts do login

--- TCC-Frontend/src/app/view/login/login.component.ts
+++ TCC-Frontend/src/app/view/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+
+import { LoginService } from '../../service/login.service';
 
 @Component({
     selector: 'app-login',
@@ -6,7 +11,39 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-    constructor() {}
+    loginForm: FormGroup;
+
+    constructor(
+        private router: Router,
+        private fb: FormBuilder,
+        private cookieService: CookieService,
+        private loginService: LoginService
+    ) {
+        this.loginForm = this.fb.group({
+            sistema: [],
+            username: ['', [Validators.required]],
+            password: ['', [Validators.required]],
+            info: [],
+        });
+    }
 
     ngOnInit(): void {}
+
+    actionLogin() {
+        this.loginForm.patchValue({
+            sistema: 'SegurancaWeb',
+            info: 'Login via angular',
+        });
+
+        this.loginService.login(this.loginForm.value).subscribe({
+            next: (v) => {
+                this.cookieService.set('tjmsauth', v.token);
+                this.router.navigate(['/home']);
+            },
+            error: (e) => {
+                console.error(e);
+            },
+            complete: () => console.info('complete'),
+        });
+    }
 }
