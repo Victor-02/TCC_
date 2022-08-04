e3727021c4e2cc45c259828c2b97d03089cb7484
Victor Casemiro <victorcasemiro2002@hotmail.com>
2022-08-03 22:53:15 -0300

    configurando rotas

--- TCC-Frontend/src/app/app-routing.module.ts
+++ TCC-Frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AgendamentoCadastrarComponent } from './view/agendamento/agendamento-cadastrar/agendamento-cadastrar.component';
 import { AgendamentoComponent } from './view/agendamento/agendamento.component';
 import { HomeComponent } from './view/home/home.component';
 
@@ -7,6 +8,7 @@ const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: HomeComponent },
     { path: 'agendamentos', component: AgendamentoComponent },
+    { path: 'agendamentos-cadastro', component: AgendamentoCadastrarComponent },
 
     {
         path: 'pacientes',
