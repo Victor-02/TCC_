9750eb257ab5b2ddf97b49aaeea9dbe6dc99d423
Victor Casemiro <victorcasemiro2002@hotmail.com>
2022-08-20 14:12:31 -0300

    criando classe controller para serviço

---
+++ TCC-Backend/src/main/java/com/tcc/tccbackend/controllers/ServicoController.java
@@ -0,0 +1,50 @@
+package com.tcc.tccbackend.controllers;
+
+import com.tcc.tccbackend.models.Servico;
+import com.tcc.tccbackend.services.ServicoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/servicos")
+public class ServicoController {
+
+    final ServicoService service;
+    private final Logger logger = LoggerFactory.getLogger(ImportController.class);
+
+    public ServicoController(ServicoService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> Insert(@Valid @RequestBody Servico servico) {
+        servico = service.save(servico);
+        logger.info("Efetuando insercão de Servico");
+        return ResponseEntity.status(HttpStatus.CREATED).body(servico);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getByID(@PathVariable Integer id) {
+        Servico servico = service.findById(id);
+        return ResponseEntity.ok().body(servico);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAll(Pageable page) {
+        Page<Servico> servicos = service.getAll(page);
+        return ResponseEntity.ok().body(servicos);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> delete(@PathVariable Integer id) {
+        service.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
