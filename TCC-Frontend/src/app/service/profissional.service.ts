bdc61c7978d93da2778723beae05c92e297f8640
Victor Casemiro <victorcasemiro2002@hotmail.com>
2022-08-26 14:43:49 -0300

    criando o service para profissional

---
+++ TCC-Frontend/src/app/service/profissional.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Profissional } from 'app/model/profissional';
+import { ResponsePageable } from 'app/shared/Paginacao';
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class ProfissionalService {
+    private readonly baseURL = 'http://localhost:8080/api';
+    private readonly endPoint = 'profissionais';
+    constructor(private httpClient: HttpClient) {}
+
+    listarProfissionais(page = 0, size = 10): Observable<ResponsePageable> {
+        const httpHeader = new HttpHeaders();
+        let pageable = new HttpParams();
+        pageable = pageable.append('page', page);
+        pageable = pageable.append('size', size);
+        return this.httpClient.get<ResponsePageable>(`${this.baseURL}/${this.endPoint}`, {
+            headers: httpHeader,
+            params: pageable,
+        });
+    }
+
+    salvar(profissional: Profissional): Observable<Profissional> {
+        return this.httpClient.post<Profissional>(`${this.baseURL}/${this.endPoint}`, profissional);
+    }
+
+    pesquisarPorId(id: number): Observable<Profissional> {
+        return this.httpClient.get<Profissional>(`${this.baseURL}/${this.endPoint}/${id}`);
+    }
+
+    delete(profissional: Profissional): Observable<{}> {
+        return this.httpClient.delete<Profissional>(`${this.baseURL}/${this.endPoint}/${profissional.id}`);
+    }
+}
